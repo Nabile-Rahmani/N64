3a31db7433881a84b3a90246f735be62ab67d9be
Nabile Rahmani <nabile13.pc@gmail.com>
2019-10-21 12:51:35 +0200

    Disable RGB dithering for full-range output in 32-bit mode.

--- RDP/32BPP/Triangle/ShadeTriangle320x240/Cycle1ShadeTriangle32BPP320X240.asm
+++ RDP/32BPP/Triangle/ShadeTriangle320x240/Cycle1ShadeTriangle32BPP320X240.asm
@@ -33,7 +33,7 @@ arch n64.rdp
   Set_Fill_Color $FFFF00FF // Set Fill Color: PACKED COLOR 32B R8G8B8A8 Pixel
   Fill_Rectangle 319<<2,239<<2, 0<<2,0<<2 // Fill Rectangle: XL 319.0,YL 239.0, XH 0.0,YH 0.0
 
-  Set_Other_Modes SAMPLE_TYPE|ALPHA_DITHER_SEL_NO_DITHER // Set Other Modes
+  Set_Other_Modes SAMPLE_TYPE | ALPHA_DITHER_SEL_NO_DITHER | RGB_DITHER_SEL_NO_DITHER // Set Other Modes
   Set_Combine_Mode $0,$00, 0,0, $6,$01, $0,$F, 1,0, 0,0,0, 4,7,7 // Set Combine Mode: SubA RGB0,MulRGB0, SubA Alpha0,MulAlpha0, SubA RGB1,MulRGB1, SubB RGB0,SubB RGB1, SubA Alpha1,MulAlpha1, AddRGB0,SubB Alpha0,AddAlpha0, AddRGB1,SubB Alpha1,AddAlpha1
 
   Sync_Pipe // Stall Pipeline, Until Preceeding Primitives Completely Finish
